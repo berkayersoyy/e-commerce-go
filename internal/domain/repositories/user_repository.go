b7270d4832caca76e6c4a809d23be7542562daca
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-04 20:06:39 +0300

    User repository interface added

---
+++ internal/domain/repositories/user_repository.go
@@ -0,0 +1,16 @@
+package repositories
+
+import (
+	"context"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/models"
+)
+
+//UserRepository User_repository
+type UserRepository interface {
+	Update(ctx context.Context, user models.User) error
+	FindByUUID(ctx context.Context, id string) (models.User, error)
+	Insert(ctx context.Context, user models.User) error
+	Delete(ctx context.Context, id string) error
+	FindByUsername(ctx context.Context, username string) (models.User, error)
+	CreateTable(ctx context.Context) error
+}
