f670361baf0857a3ff48590006cf00f81658eab4
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-04 20:07:21 +0300

    User service interface added

---
+++ internal/domain/services/user_service.go
@@ -0,0 +1,16 @@
+package services
+
+import (
+	"context"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/models"
+)
+
+//UserService User_service_dynamodb
+type UserService interface {
+	Update(ctx context.Context, user models.User) error
+	FindByUUID(ctx context.Context, id string) (models.User, error)
+	Insert(ctx context.Context, user models.User) error
+	Delete(ctx context.Context, uuid string) error
+	FindByUsername(ctx context.Context, username string) (models.User, error)
+	CreateTable(ctx context.Context) error
+}
