7586a4018941d28999a8532afc3af61d8dcb144f
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-05 03:57:59 +0300

    Product service interface added

---
+++ internal/domain/services/product_service.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+	"context"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/models"
+)
+
+type ProductService interface {
+	GetAllProducts(c context.Context) []models.Product
+	GetProductByID(c context.Context, id uint) models.Product
+	AddProduct(c context.Context, product models.Product) models.Product
+	DeleteProduct(c context.Context, product models.Product)
+}
