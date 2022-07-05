93d80fa98e2afac01bb6a756f4ce6e224a293c10
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-05 03:22:41 +0300

    User service added

---
+++ internal/application/services/user/user_service.go
@@ -0,0 +1,61 @@
+package user
+
+import (
+	"context"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/models"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/repositories"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/services"
+)
+
+type userService struct {
+	UserRepository repositories.UserRepository
+}
+
+func (u userService) Update(ctx context.Context, user models.User) error {
+	err := u.UserRepository.Update(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (u userService) FindByUUID(ctx context.Context, uuid string) (models.User, error) {
+	user, err := u.UserRepository.FindByUUID(ctx, uuid)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+func (u userService) FindByUsername(ctx context.Context, username string) (models.User, error) {
+	user, err := u.UserRepository.FindByUsername(ctx, username)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func (u userService) Insert(ctx context.Context, user models.User) error {
+	err := u.UserRepository.Insert(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (u userService) Delete(ctx context.Context, id string) error {
+	err := u.UserRepository.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (u userService) CreateTable(ctx context.Context) error {
+	err := u.UserRepository.CreateTable(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//ProvideUserService Provide user service via dynamodb
+func ProvideUserService(u repositories.UserRepository) services.UserService {
+	return userService{UserRepository: u}
+}
