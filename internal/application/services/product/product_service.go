604d1338b929ffdf6ddfad0c1bf1a9339fce16af
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-05 04:09:12 +0300

    Product service added

---
+++ internal/application/services/product/product_service.go
@@ -0,0 +1,35 @@
+package product
+
+import (
+	"context"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/models"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/repositories"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/services"
+)
+
+//productService Product service
+type productService struct {
+	ProductRepository repositories.ProductRepository
+}
+
+func (p *productService) GetAllProducts(c context.Context) []models.Product {
+	return p.ProductRepository.GetAllProducts(c)
+}
+
+func (p *productService) GetProductByID(c context.Context, id uint) models.Product {
+	return p.ProductRepository.GetProductByID(c, id)
+}
+
+func (p *productService) AddProduct(c context.Context, product models.Product) models.Product {
+	p.ProductRepository.AddProduct(c, product)
+	return product
+}
+
+func (p *productService) DeleteProduct(c context.Context, product models.Product) {
+	p.ProductRepository.DeleteProduct(c, product)
+}
+
+// ProvideProductService Provide product service
+func ProvideProductService(p repositories.ProductRepository) services.ProductService {
+	return &productService{ProductRepository: p}
+}
---
+++ internal/infrastructure/repositories/product/product_repository.go
@@ -0,0 +1,74 @@
+package product
+
+import (
+	"context"
+	"fmt"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/models"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/repositories"
+	"github.com/jinzhu/gorm"
+	"github.com/sethvargo/go-retry"
+	"log"
+	"os"
+	"time"
+)
+
+//productRepository Product repository
+type productRepository struct {
+	DB *gorm.DB
+}
+
+func (p *productRepository) GetAllProducts(c context.Context) []models.Product {
+	var products []models.Product
+	p.DB.Find(&products)
+	return products
+}
+
+func (p *productRepository) GetProductByID(c context.Context, id uint) models.Product {
+	var product models.Product
+	p.DB.First(&product, id)
+	return product
+}
+
+func (p *productRepository) AddProduct(c context.Context, product models.Product) models.Product {
+	p.DB.Save(&product)
+	return product
+}
+
+func (p *productRepository) DeleteProduct(c context.Context, product models.Product) {
+	p.DB.Delete(&product)
+}
+
+//mysqlClient Mysql client
+type mysql struct {
+	Client *gorm.DB
+}
+
+//initDb Init db
+func initDb() *gorm.DB {
+	dsn := os.Getenv("MYSQL_DSN")
+	ctx := context.Background()
+	var db *gorm.DB
+	var err error
+	if err := retry.Fibonacci(ctx, 1*time.Second, func(ctx context.Context) error {
+		db, err = gorm.Open("mysql", dsn)
+		if err != nil {
+			fmt.Println(err)
+			return retry.RetryableError(err)
+		}
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	db.DB().SetMaxOpenConns(10)
+	db.DB().SetMaxIdleConns(5)
+
+	db.AutoMigrate(&models.Product{})
+
+	return db
+}
+
+//ProvideProductRepository Provide product repository
+func ProvideProductRepository() repositories.ProductRepository {
+	return &productRepository{DB: initDb()}
+}
