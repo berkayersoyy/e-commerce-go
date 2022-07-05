59bcda049bdd3e8af1fd44700e222a52e39112a5
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-05 03:58:29 +0300

    Product handler interface added

---
+++ internal/domain/handlers/product_handler.go
@@ -0,0 +1,12 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+//ProductHandler Product handler
+type ProductHandler interface {
+	GetAllProducts(c *gin.Context)
+	GetProductByID(c *gin.Context)
+	AddProduct(c *gin.Context)
+	UpdateProduct(c *gin.Context)
+	DeleteProduct(c *gin.Context)
+}
