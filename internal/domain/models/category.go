202614ca1fe1d2c77bfcba3232d99b761b2be30d
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-06 16:03:33 +0300

    Category entity added & configuration with product entity added

---
+++ internal/domain/models/category.go
@@ -0,0 +1,12 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+const (
+	CategoryNotFound = "category not found"
+)
+
+type Category struct {
+	gorm.Model
+	Name string `json:"name" validate:"required,min=5,max=45"`
+}
--- internal/domain/models/product.go
+++ internal/domain/models/product.go
@@ -10,5 +10,6 @@ type Product struct {
 	gorm.Model
 	Name        string  `json:"name" validate:"required,min=5,max=45"`
 	Price       float32 `json:"price" validate:"required"`
+	CategoryId  uint    `json:"categoryId" validate:"required"`
 	Description string  `json:"description" validate:"required"`
 }
