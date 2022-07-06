3681ed308e82bf6ad68a4a5e2600bb2bf9c82c30
berkayersoyy <berkay.ersoy.007@gmail.com>
2022-07-06 14:25:53 +0300

    Authentication middleware added

---
+++ internal/presentation/middlewares/auth_middleware.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/berkayersoyy/e-commerce-go/internal/domain/services"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//AuthorizeJWTMiddleware Jwt middleware
+func AuthorizeJWTMiddleware(a services.AuthService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := a.ValidateToken(c.Request)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			fmt.Println(claims)
+		} else {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+	}
+}
